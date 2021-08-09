8c9028b4ead00ce09e05dd8b5cd4c3ec84ebfeaf
ggunes18 <56797328+ggunes18@users.noreply.github.com>
2021-08-09 17:46:56 +0300

    Add files via upload

---
+++ CaseStudyProject/CaseStudyProject/Book Data/Feed.swift
@@ -0,0 +1,12 @@
+//
+//  Feed.swift
+//  CaseStudyProject
+//
+//  Created by Gizem on 28.07.2021.
+//
+
+import Foundation
+
+struct Feed: Codable {
+    var feed : Results
+}
---
+++ CaseStudyProject/CaseStudyProject/Book Data/Results.swift
@@ -0,0 +1,12 @@
+//
+//  Results.swift
+//  CaseStudyProject
+//
+//  Created by Gizem on 28.07.2021.
+//
+
+import Foundation
+
+struct Results: Codable {
+    var results : [Book]
+}
